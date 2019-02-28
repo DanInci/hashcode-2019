7d9e8a2b86ccf85bf3010c38a342fb13f42420f7
Daniel Incicau <daniel.incicau@busymachines.com>
2019-02-28 22:18:28 +0200

    Integrated sorting with vertical photos merge

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
-//import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import model.Photo;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    private static Photo[] photos;
-    private static int photoNo;
-    private static int slideNo;
+    private static List<Photo> horizontalPhotos = new ArrayList<Photo>();
+    private static int horizontalPhotosNo;
+
+    private static List<Photo> verticalPhotos = new ArrayList<Photo>();
+    private static int verticalPhotosNo;
+
 
     public static void main(String[] args) throws IOException {
         if(args.length == 0) {
@@ -22,82 +22,91 @@ public class Main {
         }
         for(String arg: args) {
             readInput(arg);
-            test();
-            //writeOutput(arg);
+            groupVerticalIntoHorizontal();
+            optimze();
+            writeOutput(arg);
         }
-    }
-    
-    private static void test() {
-    	for (int i = 0; i < photoNo - 1; i++) {
-    		int maxPhoto = i;
-    		int maxScore = photos[i].compareTo(photos[i + 1]);
-    		int score;
-    		
-    		for (int j = i + 2; j < photoNo; j++) {
-    			score = photos[i].compareTo(photos[j]);
-    			if (score > maxScore) { 
-    				maxScore = score;
-    				maxPhoto = j;
-    			}
-    		}
-    		
-    		Photo p = photos[i + 1];
-    		photos[i + 1] = photos[maxPhoto];
-    		photos[maxPhoto] = p;
-    	}
-    	
-    	for (Photo p : photos) 
-    		System.out.println(p);
     }
 
     private static void readInput(String fileName) throws IOException {
         File file = new File("input/" + fileName);
         Scanner scanner = new Scanner(file);
 
-        photoNo = scanner.nextInt();
-        photos = new Photo[photoNo];
+        verticalPhotosNo = scanner.nextInt();
         scanner.nextLine();
 
-        for (int i=0; i<photoNo; i++) {
+        for (int i=0; i<verticalPhotosNo; i++) {
             String photoLine = scanner.nextLine();
             String[] photoDetails = photoLine.split(" ");
-            int orientation = photoDetails[0].equals("H") ? 0 : 1;
+            String orientation = photoDetails[0];
             int tagsNo = Integer.parseInt(photoDetails[1]);
             String[] tags = new String[tagsNo];
             System.arraycopy(photoDetails, 2, tags, 0, tagsNo);
-            photos[i] = new Photo(i, orientation, tagsNo, tags);
-            System.out.println("Read: " + photos[i]);
+            if(orientation.equals("H")) {
+                Photo photo = new Photo(String.valueOf(i), tagsNo, tags);
+                horizontalPhotosNo++;
+                horizontalPhotos.add(photo);
+                System.out.println("Read horizontal: " + photo);
+            } else {
+                Photo photo = new Photo(String.valueOf(i), tagsNo, tags);
+                verticalPhotos.add(photo);
+                System.out.println("Read vertical: " + photo);
+
+            }
+
         }
-        
-        scanner.close();
+        verticalPhotosNo -= horizontalPhotosNo;
+    }
+
+    private static void groupVerticalIntoHorizontal() {
+        int length = verticalPhotosNo % 2 == 0 ? verticalPhotosNo : verticalPhotosNo - 1;
+        int i = 0;
+        Collections.shuffle(verticalPhotos);
+        while (i < length) {
+            Photo firstPhoto = verticalPhotos.get(i);
+            Photo secondPhoto = verticalPhotos.get(i + 1);
+            String id = firstPhoto.getId() + " " + secondPhoto.getId();
+            HashSet<String> tags = new HashSet<>();
+            tags.addAll(Arrays.asList(firstPhoto.getTags()));
+            tags.addAll(Arrays.asList(secondPhoto.getTags()));
+            int tagsIntersectionNo = tags.size();
+            Photo combinedPhoto = new Photo(id, tagsIntersectionNo, tags.toArray(new String[tagsIntersectionNo]));
+            horizontalPhotosNo++;
+            horizontalPhotos.add(combinedPhoto);
+            i += 2;
+        }
+    }
+
+    private static void optimze() {
+        for (int i = 0; i < horizontalPhotosNo - 1; i++) {
+            Photo currentPhoto = horizontalPhotos.get(i);
+            int maxPhotoIndex = i+1;
+            int maxScore = currentPhoto.compareTo(horizontalPhotos.get(i+1));
+
+            for (int j = i + 2; j < horizontalPhotosNo; j++) {
+                int score = currentPhoto.compareTo(horizontalPhotos.get(j));
+                if (score > maxScore) {
+                    maxScore = score;
+                    maxPhotoIndex = j;
+                }
+            }
+
+            if(maxPhotoIndex != i+1) {
+                Photo aux = horizontalPhotos.get(i+1);
+                Photo maxPhoto = horizontalPhotos.get(maxPhotoIndex);
+                horizontalPhotos.set(i+1, maxPhoto);
+                horizontalPhotos.set(maxPhotoIndex, aux);
+            }
+        }
     }
 
     private static void writeOutput(String fileName) throws IOException {
         File file = new File("output/" + fileName);
         BufferedWriter bf = new BufferedWriter(new FileWriter(file));
-        int slideNo = 0;
-        HashSet<Integer> ommitedIds = new HashSet<>();
-        StringBuilder builder = new StringBuilder();
-        for(int i=0;i<photoNo;i++) {
-            if(ommitedIds.contains(i)) {
-                continue;
-            }
-            Photo photo1 = photos[i];
-            Photo photo2 = null;
-            if(photo1.getOrientation() == 1) {
-                int j=i+1;
-                while(j < photoNo && photos[j].getOrientation() == 0 && ommitedIds.contains(j)) {
-                    j++;
-                }
-                if(j < photoNo) {
-                    photo2 = photos[j];
-                    ommitedIds.add(j);
-                }
-            }
-            builder.append(photo2 != null ? photo1.getId() + " " + photo2.getId() + "\n" : photo1.getId() + "\n");
-            slideNo++;
+        bf.write(horizontalPhotosNo + "\n");
+        for(Photo photo : horizontalPhotos) {
+            bf.write(photo.getId() + "\n");
         }
-        bf.write(slideNo + "\n" + builder.toString());
         bf.close();
         System.out.println("Write completed");
     }
--- src/model/Photo.java
+++ src/model/Photo.java
@@ -1,5 +1,6 @@
 package model;
 
+
 import java.util.Collections;
 import java.util.HashSet;
 
@@ -9,52 +10,46 @@ import java.util.HashSet;
  */
 public class Photo implements Comparable<Photo> {
 
-    private int id;
-    private int orientation;
+    private String id;
     private int tagsNo;
     private String[] tags;
 
-    public Photo(int id, int orientation, int tagsNo, String[] tags) {
+    public Photo(String id, int tagsNo, String[] tags) {
         this.id = id;
-        this.orientation = orientation;
         this.tagsNo = tagsNo;
         this.tags = tags;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public int getOrientation() {
-        return orientation;
+    public int getTagsNo() {
+        return tagsNo;
     }
 
     public String[] getTags() {
         return tags;
     }
-    
-    public int getTagsNo() {
-    	return tagsNo;
-    }
 
     @Override
     public String toString() {
-        return "Photo id: " + id + ", orientation: " + (orientation == 0 ? "H" : "V") + ", tagsNo: " + tagsNo;
+        return "Photo id: " + id + ", tagsNo: " + tagsNo;
     }
 
-	@Override
-	public int compareTo(Photo p) {
-		HashSet<String> set = new HashSet<String>();
-		Collections.addAll(set, this.tags);
-		
-		int intersectionCount = 0;
-		
-		for (int i = 0; i < p.tagsNo; i++) {
-			if (set.contains(p.tags[i])) {
-				intersectionCount ++;
-			}
-		}
-		
-		return Math.min(Math.min(intersectionCount, this.tagsNo - intersectionCount), p.tagsNo - intersectionCount);
-	}
+    @Override
+    public int compareTo(Photo p) {
+        HashSet<String> set = new HashSet<String>();
+        Collections.addAll(set, this.tags);
+
+        int intersectionCount = 0;
+
+        for (int i = 0; i < p.tagsNo; i++) {
+            if (set.contains(p.tags[i])) {
+                intersectionCount ++;
+            }
+        }
+
+        return Math.min(Math.min(intersectionCount, this.tagsNo - intersectionCount), p.tagsNo - intersectionCount);
+    }
 }
