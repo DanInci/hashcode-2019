99a6f22df3eb8d0e35a587ffaaaaf21038926a55
User <User@DESKTOP-682NJGO>
2019-02-28 20:04:37 +0200

    test adam 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
     	System.out.println("Test2");
-    	System.out.println("Test Adam");
         System.out.println("Hello World!");
+        System.out.println("Test Adam");
     }
 }
