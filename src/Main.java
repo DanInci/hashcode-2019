89f8be038c9e17ead7f34f7e3b0ab0684f0128df
Daniel Incicau <daniel.incicau@busymachines.com>
2019-02-28 21:02:35 +0200

    Implemented write output

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import model.Photo;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
 
     private static Photo[] photos;
+    private static int photoNo;
+    private static int slideNo;
 
     public static void main(String[] args) throws IOException {
         if(args.length == 0) {
@@ -15,6 +21,7 @@ public class Main {
         }
         for(String arg: args) {
             readInput(arg);
+            writeOutput(arg);
         }
     }
 
@@ -22,7 +29,7 @@ public class Main {
         File file = new File("input/" + fileName);
         Scanner scanner = new Scanner(file);
 
-        int photoNo = scanner.nextInt();
+        photoNo = scanner.nextInt();
         photos = new Photo[photoNo];
         scanner.nextLine();
 
@@ -36,6 +43,35 @@ public class Main {
             photos[i] = new Photo(i, orientation, tagsNo, tags);
             System.out.println("Read: " + photos[i]);
         }
+    }
 
+    private static void writeOutput(String fileName) throws IOException {
+        File file = new File("output/" + fileName);
+        BufferedWriter bf = new BufferedWriter(new FileWriter(file));
+        int slideNo = 0;
+        HashSet<Integer> ommitedIds = new HashSet<>();
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<photoNo;i++) {
+            if(ommitedIds.contains(i)) {
+                continue;
+            }
+            Photo photo1 = photos[i];
+            Photo photo2 = null;
+            if(photo1.getOrientation() == 1) {
+                int j=i+1;
+                while(j < photoNo && photos[j].getOrientation() == 0 && ommitedIds.contains(j)) {
+                    j++;
+                }
+                if(j < photoNo) {
+                    photo2 = photos[j];
+                    ommitedIds.add(j);
+                }
+            }
+            builder.append(photo2 != null ? photo1.getId() + " " + photo2.getId() + "\n" : photo1.getId() + "\n");
+            slideNo++;
+        }
+        bf.write(slideNo + "\n" + builder.toString());
+        bf.close();
+        System.out.println("Write completed");
     }
 }
